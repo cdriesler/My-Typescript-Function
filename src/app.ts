ca481c9ca3cdba867511edc50f0e6dee25f2b320
Chuck Driesler <cdriesler.iv@gmail.com>
2025-03-12 20:47:29 +0000

    reading args

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
+import { readFileSync } from 'node:fs'
 import yargs from 'yargs'
 
 const argv = yargs(process.argv).argv
 
+console.log('???')
 console.log(argv)
+
+const data = JSON.parse(readFileSync('/speckle/automate.json', 'utf-8'))
+
+console.log(data)
+
